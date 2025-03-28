f9bbd9fdaa145f960b2fba7c8b09b6329ff6397a
unknown <farismmk@gmail.com>
2025-03-28 03:13:46 -0700

    use xbuild

---
+++ build.ts
@@ -0,0 +1,39 @@
+import { type Build } from 'xbuild';
+
+const build: Build = {
+    common: {
+        project: 'fastlz',
+        archs: ['x64'],
+        variables: [],
+        copy: {},
+        defines: [],
+        options: [],
+        subdirectories: [],
+        libraries: {
+            fastlz: {
+                sources: ['FastLZ/fastlz.c']
+            },
+        },
+        buildDir: 'build',
+        buildOutDir: 'libs',
+        buildFlags: []
+    },
+    platforms: {
+        win32: {
+            windows: {
+                buildFlags: ['-T ClangCl']
+            },
+            android: {
+                archs: ['x86', 'x86_64', 'armeabi-v7a', 'arm64-v8a'],
+            }
+        },
+        linux: {
+            linux: {}
+        },
+        darwin: {
+            macos: {}
+        }
+    }
+}
+
+export default build;
